64af9c6dde1dab1c37c51b573b69b2c27678b7d6
Angelina <lyubyatinskaya06@bk.ru>
2024-12-06 10:54:39 +0700

    версия 2

--- lab/Source.cpp
+++ lab/Source.cpp
@@ -17,8 +17,6 @@ int main()
 	uint16_t xabs = abs(x) | ((x >> 15) << 15); //Переменная для хранения модуля
 	cout << "Прямой код: " << xabs << " | " << hex << xabs << " | " << oct << xabs << " | " << dec << bitset<16>(xabs) << endl;
 	xabs = (~xabs) | ((x >> 15) << 15); // Обратный код
-	cout << "Обратный код: " << xabs << " | " << hex << xabs << " | " << oct << xabs << " | " << dec << bitset<16>(xabs) << endl;
-	(xabs++) | ((x >> 15) << 15); //Доп. код
-	cout << "Дополнительный код: " << xabs << " | " << hex << xabs << " | " << oct << xabs << " | " << dec << bitset<16>(xabs) << endl;
+	
 	return 0;
 }
